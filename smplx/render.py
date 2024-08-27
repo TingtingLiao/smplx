8222228098770089b0c381459a23c369382f8c6c
TingtingLiao <tingtingliao000@gmail.com>
2024-08-27 15:39:16 +0400

    update render

--- smplx/render.py
+++ smplx/render.py
@@ -2,6 +2,7 @@ import os
 import random 
 import torch
 import torch.nn.functional as F
+import numpy as np
 import nvdiffrast.torch as dr
  
 
@@ -28,6 +29,9 @@ class Renderer(torch.nn.Module):
         else:
             self.glctx = dr.RasterizeGLContext()
 
+        self.light_dir = np.array([0, 0])
+        self.ambient_ratio = 0.5
+        
         if shading and hdr_path is not None:
             import envlight
             if hdr_path is None:
@@ -40,7 +44,6 @@ class Renderer(torch.nn.Module):
 
     def render_bg(self, envmap_path):
         '''render with shading'''
-     
         from PIL import Image 
         envmap = Image.open(envmap_path)
         h,w = self.res
@@ -58,24 +61,51 @@ class Renderer(torch.nn.Module):
         self.bgs = dr.texture(envmap[None, ...], envmap_uvs, filter_mode='linear').flip(1)
         self.bgs[..., -1] = 0 # Set alpha to 0
 
-    def shading(self, mesh, rast, rast_db, mode='rgb'): 
-        if mesh.albedo is not None:
-            texc, texc_db = dr.interpolate(mesh.vt[None, ...], rast, mesh.ft, rast_db=rast_db, diff_attrs='all') 
-            albedo = dr.texture(
-                mesh.albedo.unsqueeze(0), texc, uv_da=texc_db, filter_mode='linear-mipmap-linear')  # [B, H, W, 3]
-            color = torch.where(rast[..., 3:] > 0, albedo, torch.tensor(0).to(albedo.device))  # remove background
-        elif mesh.vc is not None:
-            color, _ = dr.interpolate(mesh.vc[None, ..., :3].contiguous().float(), rast, mesh.f)
-        else:
-            return None 
+    def shading(self, albedo, normal=None, mode='albedo'):  
+        if mode == "albedo":
+            return albedo 
+        elif mode == "lambertian":
+            assert normal is not None, "normal and light direction should be provided" 
+            light_d = np.deg2rad(self.light_dir)
+            light_d = np.array([
+                np.cos(light_d[0]) * np.sin(light_d[1]),
+                -np.sin(light_d[0]),
+                np.cos(light_d[0]) * np.cos(light_d[1]),
+            ], dtype=np.float32)
+            light_d = torch.from_numpy(light_d).to(albedo.device)
+            lambertian = self.ambient_ratio + (1 - self.ambient_ratio)  * (normal @ light_d).float().clamp(min=0)
+            albedo = albedo * lambertian.unsqueeze(-1) 
+            return albedo
+              
+        elif mode == "pbr": 
+            xyzs, _ = dr.interpolate(self.mesh.v.unsqueeze(0), rast, self.mesh.f) # [1, H, W, 3]
+            viewdir = safe_normalize(xyzs - pose[:3, 3])
+
+            n_dot_v = (normal * viewdir).sum(-1, keepdim=True) # [1, H, W, 1]
+            reflective = n_dot_v * normal * 2 - viewdir
+
+            diffuse_albedo = (1 - metallic) * albedo
+
+            fg_uv = torch.cat([n_dot_v, roughness], -1).clamp(0, 1) # [H, W, 2]
+            fg = dr.texture(
+                self.FG_LUT,
+                fg_uv.reshape(1, -1, 1, 2).contiguous(),
+                filter_mode="linear",
+                boundary_mode="clamp",
+            ).reshape(1, H, W, 2)
+            F0 = (1 - metallic) * 0.04 + metallic * albedo
+            specular_albedo = F0 * fg[..., 0:1] + fg[..., 1:2]
+
+            diffuse_light = self.light(normal)
+            specular_light = self.light(reflective, roughness)
+
+            color = diffuse_albedo * diffuse_light + specular_albedo * specular_light # [H, W, 3]
+            color = color * alpha + self.bg_color * (1 - alpha)
+
+            buffer = color[0].detach().cpu().numpy()
+
         
-        if mode == "lambertian":
-            lambertian = ambient_ratio + (1 - ambient_ratio) * (normal @ light_d.view(-1, 1)).float().clamp(min=0)
-            color = color * lambertian.repeat(1, 1, 1, 3)
-        elif mode == "pbr":
-            pass 
-
-
+        return color 
 
     def forward(self, mesh, mvp,
                 h=512,
@@ -98,9 +128,7 @@ class Renderer(torch.nn.Module):
             mode: str rendering type rgb, normal, lambertian
         Returns:
             color: [batch, h, w, 3]
-            alpha: [batch, h, w, 1]
-            depth: [batch, h, w, 1]
-
+            alpha: [batch, h, w, 1] 
         """
         B = mvp.shape[0] 
         v_clip = torch.bmm(F.pad(mesh.v, pad=(0, 1), mode='constant', value=1.0).unsqueeze(0).expand(B, -1, -1),
@@ -111,36 +139,31 @@ class Renderer(torch.nn.Module):
         res = (int(h * spp), int(w * spp)) if spp > 1 else (h, w)
         rast, rast_db = dr.rasterize(self.glctx, v_clip, mesh.f, res)
 
-        ################################################################################
+        ############################### 
         # Interpolate attributes
-        ################################################################################
-
+        ############################### 
+        
         # Interpolate world space position
-        alpha, _ = dr.interpolate(torch.ones_like(v_clip[..., :1]), rast, mesh.f)  # [B, H, W, 1]
-        depth = rast[..., [2]]  # [B, H, W]
- 
-    
+        alpha, _ = dr.interpolate(torch.ones_like(v_clip[..., :1]), rast, mesh.f)  # [B, H, W, 1] 
+        normal, _ = dr.interpolate(mesh.vn[None, ...].float(), rast, mesh.f)
+        normal = (normal + 1) / 2.
+        
         if mesh.albedo is not None:
             texc, texc_db = dr.interpolate(mesh.vt[None, ...], rast, mesh.ft, rast_db=rast_db, diff_attrs='all') 
             albedo = dr.texture(
-                mesh.albedo.unsqueeze(0), texc, uv_da=texc_db, filter_mode='linear-mipmap-linear')  # [B, H, W, 3]
-            color = torch.where(rast[..., 3:] > 0, albedo, torch.tensor(0).to(albedo.device))  # remove background
+                mesh.albedo.unsqueeze(0), texc, uv_da=texc_db, filter_mode='linear')  # [B, H, W, 3] 
         elif mesh.vc is not None:
             color, _ = dr.interpolate(mesh.vc[None, ..., :3].contiguous().float(), rast, mesh.f)
         else:
-            color = None 
-        
-        if mode == "lambertian" and color is not None:
-            lambertian = ambient_ratio + (1 - ambient_ratio) * (normal @ light_d.view(-1, 1)).float().clamp(min=0)
-            color = color * lambertian.repeat(1, 1, 1, 3)
-
-        # render vertex color  
-        if mesh.vn is None:
-            mesh.auto_normal()
-
-        normal, _ = dr.interpolate(mesh.vn[None, ...].float(), rast, mesh.f)
-        normal = (normal + 1) / 2.
-
+            color = None
+         
+        ############################### 
+        # shading 
+        ###############################
+        if color is not None:
+            color = torch.where(rast[..., 3:] > 0, color, torch.tensor(0).to(color.device))  # remove background
+            color = self.shading(color, normal, mode='pbr')
+            
         # antialias
         normal = dr.antialias(normal, rast, v_clip, mesh.f).clamp(0, 1)  # [H, W, 3]
         alpha = dr.antialias(alpha, rast, v_clip, mesh.f).clamp(0, 1)  # [H, W, 3]

be0c82a3379664b4ff4315c6dd69639977517102
TingtingLiao <tingtingliao000@gmail.com>
2024-09-03 21:37:54 +0400

    vis lbs weight: add cmap choice

--- smplx/vis.py
+++ smplx/vis.py
@@ -2,18 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
-def lbs_weights_to_color(lbs_weights):
+def lbs_weights_to_colors(lbs_weights, cmap='Pastel1'):
     '''
     map lbs weights to rgb colors 
-    
+
     Args: 
         lbs_weights: torch.tensor [N, J]
-    
+        cmap: str, matplotlib colormap name
+            find more choice from https://matplotlib.org/stable/users/explain/colors/colormaps.html  
     Returns:
         colors: torch.tensor [N, 3]
     ''' 
     J = lbs_weights.shape[1] 
-    J_color = plt.cm.rainbow(np.linspace(0, 1, num_colors))
-    J_ids = lbs_weights.argmax(dim=1) 
-    colors = J_color[max_idx]
+    
+    mapper = getattr(plt.cm, cmap)
+    J_colors = mapper(np.linspace(0, 1, J))
+    J_ids = lbs_weights.argmax(dim=1)  
+    colors = J_colors[J_ids]  
     return colors
